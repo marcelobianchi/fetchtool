5b4c5f36c404cbcdc733fd172527d26d94ae6099
Marcelo Bianchi <m.tchelo@gmail.com>
2014-07-02 20:22:39 -0300

    Implemented request based on fdsn -> events and arclink -> metadata

--- RequestBuilderArclink.py
+++ RequestBuilderArclink.py
@@ -1,11 +1,12 @@
 ## Python Basics
 import sys, pickle
 
-from RequestBuilder import BaseBuilder, Range, AreaRange
+from RequestBuilder import BaseBuilder, Range, AreaRange, NextItem
 
 ## Obspy tools
 from obspy.core import UTCDateTime, util, AttribDict, read as oREAD, Stream
-from obspy import fdsn, taup, arclink, sh
+from obspy import fdsn
+from obspy.fdsn.header import FDSNException
 
 ## Sc3 Tools
 from seiscomp.arclink.manager import ArclinkManager, ArclinkError
@@ -38,186 +39,64 @@ def unWrapNSLC(objs, archive = None, onlyShared = False):
 class ArcLinkRequestBuilder(BaseBuilder):
     def __init__(self, fdsnURL, arclinkURL):
         (host,port,user) = arclinkURL.strip().split(":")
-        self._am = ArclinkManager("%s:%s" % (host,port), user)
-        self._fc = fdsn.Client(fdsnURL)
+        self.arclink_manager = ArclinkManager("%s:%s" % (host,port), user)
+        self.fdsn_client = fdsn.Client(fdsnURL)
 
-    '''
-    To clean methods
-    '''
-    def _loadInventory(self):
-        print >>sys.stderr,"Loading Complete Inventory ..."
-        inv = self.am.get_inventory("*", "*", "*", "*", t0.datetime, t1.datetime)
-        print >>sys.stderr," filtering inventory ..."
-        self._filter(inv)
-        print >>sys.stderr,"Inventory if ready.\n"
-        return inv
+    def __chooseArcLink(self, item, location, channel, targetsps, instcode):
 
-    def _preparePattern(self, pattern):
-        return "^" + pattern.replace(".","[.]").replace("*",".*").replace("?",".") + "$"
-
-    def _filter(self, inv):
-        if self.selectors == None: return
-
-        for (ncode, nstart, net) in  unWrapNSLC(inv.network):
-            for (scode, sstart, sta) in  unWrapNSLC(net.station):
-                code = "%s.%s" % (ncode,scode)
-                for pattern in self.selectors:
-                    if re.match(self._preparePattern(pattern), code) is not None:
-                        break
-                else:
-                    net.remove_station(scode, sstart)
-
-    def __ArclinkChoose(self, item, location, channel, targetsps, instcode):
-
-        newccode = channel.code
-        newsps = (channel.sampleRateNumerator / channel.sampleRateDenominator)
-        newitem = ( (channel.code, channel.start, channel.azimuth, channel.dip), (location.code, location.start), abs(targetsps - newsps))
+        # Build a newitem for the current channel
+        sps = channel.sampleRateNumerator / channel.sampleRateDenominator
+        newitem = (location.code, channel.code, sps, channel.azimuth, channel.dip, abs(sps - targetsps))
 
         # Check that the instrument code is allowed to select
-        if channel.code[1] not in instcode:
-            return item
+        if channel.code[1] not in instcode: return item
 
         # If we did not select yet return the current
-        if item[0] == None:
-            return newitem
-
-        index = item[2]
-        ccode  = item[0][0]
+        if item[0] == None: return newitem
 
         # Decided based on SPS first
-        if abs(targetsps - newsps) == index:
+        if newitem[5] == item[5]:
             # And based on the Channel Instrument Code
-            if instcode.index(newccode[1]) < instcode.index(ccode[1]):
+            if instcode.index(newitem[1][1]) < instcode.index(item[1][1]):
                 return newitem
-        elif abs(targetsps - newsps) < index:
+        elif newitem[5] < item[5] and newitem[5] > targetsps:
             return newitem
 
         return item
 
-    def __getArclinkChannelList(self, station, tp, targetsps, instcode):
+    def __getArcLinkChannelList(self, station, t0, targetsps, instcode):
         clist = [ ]
 
-        z = (None, None, None)
-        n = (None, None, None)
-        e = (None, None, None)
+        ## Choose will build something like: (loca, chan, sps , az  , dip , dsps)
+        z = (None, None, None, None, None, None)
+        n = (None, None, None, None, None, None)
+        e = (None, None, None, None, None, None)
 
-        for (lcode, lstart, location) in unWrapNSLC(station.sensorLocation):
-            for (ccode, cstart, channel) in unWrapNSLC(location.stream):
-
-                # Check time range
-                if (tp.datetime < channel.start) or (channel.end is not None and tp.datetime > channel.end):
+        for (lcode,lstart,location) in unWrapNSLC(station.sensorLocation):
+            for (ccode,cstart,channel) in unWrapNSLC(location.stream):
+                if t0 < channel.start or (channel.end is not None and t0 > channel.end):
                     continue
+    
+                if channel.code[-1] == "Z":
+                    z = self.__chooseArcLink(z, location, channel, targetsps, instcode)
+                elif channel.code[-1] == "N" or channel.code[-1] == "1":
+                    n = self.__chooseArcLink(n, location, channel, targetsps, instcode)
+                elif channel.code[-1] == "E" or channel.code[-1] == "2":
+                    e = self.__chooseArcLink(e, location, channel, targetsps, instcode)
+    #             else:
+    #                 print >>sys.stderr, "Unknow channel %s.%s" % (channel.location_code, channel.code)
 
-                # Enter selection of channel
-                if ccode[-1] == "Z":
-                    z = self._choose(z, location, channel, targetsps, instcode)
-                elif ccode[-1] == "N" or ccode[-1] == "1":
-                    n = self._choose(n, location, channel, targetsps, instcode)
-                elif ccode[-1] == "E" or ccode[-1] == "2":
-                    e = self._choose(e, location, channel, targetsps, instcode)
-                else:
-                    print >>sys.stderr, "Unknow channel %s.%s" % (lcode, ccode)
+        if z[1] is None or n[1] is None or e[1] is None:
+            raise NextItem("No Z, N or E channel(s) found")
 
-        if z[0] is not None:
-            clist.append((z[1][0], z[0][0], z[0][2], z[0][3]))
+        # Return only (location, channel, azimuth, dip)
+        z = (z[0], z[1], z[3], z[4])
+        n = (n[0], n[1], n[3], n[4])
+        e = (e[0], e[1], e[3], e[4])
 
-        if n[0] is not None:
-            clist.append((n[1][0], n[0][0], n[0][2], n[0][3]))
+        return (z,n,e)
 
-        if e[0] is not None:
-            clist.append((e[1][0], e[0][0], e[0][2], e[0][3]))
-
-        return clist
-
-    def _checkEvent(self, event):
-
-        if len(event.origins) == 0:
-            print >>sys.stderr,"  Event with no origin"
-            return False
-
-        origin = event.preferred_origin()
-        if origin is None:
-            origin = event.origins[0]
-
-        if origin.depth is None: 
-            print >>sys.stderr,"  Origin has no depth"
-            return False
-
-        return True
-
-    def getDefaultsParameters(self):
-        parameters = AttribDict({})
-
-        # Channel Selections
-        parameters['targetsps'] = 20.0
-        parameters['prewindow'] = 2.0 * MINUTE
-        parameters['postwindow'] = 15.0 * MINUTE
-        parameters['instcode'] = ["H", "L"]
-
-        # Event Selection
-        parameters['minradius'] = 0.0
-        parameters['maxradius'] = 90.0
-        parameters['minmagnitude'] = 5.5
-        return parameters
-
-    def OLDstationBased(self, parameters):
-        request = {}
-
-        print >>sys.stderr,"StationBuilder:"
-        for (ncode, nstart, network) in unWrapNSLC(self.inventory.network):
-            for (scode, sstart, station) in unWrapNSLC(network.station):
-                lines = []
-                print >>sys.stderr," Working on %s.%s (" % (ncode,scode),
-
-                # Build Station Info
-                stationinfo = self._buildStationInfo(network, station)
-
-                # Fetch Events
-                catalog = self._events(station, parameters)
-                if not catalog:
-                    print >>sys.stderr,"No Events)"
-                    continue
-                else:
-                    print >>sys.stderr,"%d events)" % (len(catalog))
-
-                # Loop on Event
-                for event in catalog:
-                    if not self._checkEvent(event): 
-                        print >>sys.stderr,"  skipping event %s" % event
-                        print event.origins
-                        continue
-
-                    eventinfo = self._buildEventInfo(event)
-
-                    # Compute arrival time
-                    (tp, slowness) = self._arrivalTime(station, event)
-                    if tp == None:
-                        print >>sys.stderr,"No matching phase for %s on %s" % (event, station.code)
-
-                    # Find matching channel close to SPS
-                    channels = self._getChannelList(station, tp, parameters.targetsps, parameters.instcode)
-                    if len(channels) == 0:
-                        print >>sys.stderr,"  no channels operating at event time = %s" % tp
-                        continue
-
-                    # Build pick info
-                    pickinfo = self._buildPickInfo("P", tp, slowness)
-
-                    lines.append((tp - parameters.prewindow, tp + parameters.postwindow, ncode, scode, channels, stationinfo, eventinfo, pickinfo))
-
-                if lines:
-                    request["%s.%s" % (ncode, scode)] = lines
-                else:
-                    print >>sys.stderr,"Will not request any data for %s.%s" % (ncode,scode)
-
-        print >>sys.stderr,"StationBuilder is ready.\n"
-        ## { STKEY: (T0,T1,N,S,[(LO,C)],At-Station, At-Event, At-Pick) }
-        return request
-
-    '''
-    New Methods
-    '''
-    def eventBased(self, t0, t1, targetSamplingRate, allowedGainCodes, timeRange,
+    def eventBased(self, t0, t1, targetSamplingRate, allowedGainCodes, timeRange, phasesOrPhaseGroup,
                    eventRestrictionArea,
                    magnitudeRange,
                    depthRange,
@@ -227,8 +106,13 @@ class ArcLinkRequestBuilder(BaseBuilder):
                    distanceRange = None
                    ):
 
-        # List of request lines
         lines = []
+
+        (phasename, phaselist) = self.resolve_phasenames(phasesOrPhaseGroup)
+        print >>sys.stderr,"Searching using: %s %s" % (phasename, phaselist)
+
+        if networkStationCodes is None:
+            networkStationCodes = [ "*.*" ]
 
         kwargsevent = self.fill_kwargsevent(t0, t1,
                                               eventRestrictionArea,
@@ -237,22 +121,66 @@ class ArcLinkRequestBuilder(BaseBuilder):
                                               None, None, None)
 
         try:
-            events = self._fc.get_events(**kwargsevent)
-            events.plot()
+            events = self.fdsn_client.get_events(**kwargsevent)
             print >>sys.stderr,"Found %d events." % len(events)
-        except fdsn.header.FDSNException:
-            print >>sys.stderr,"\nNo events found for the given parameters.\n"
-            sys.exit()
+        except FDSNException:
+            print >>sys.stderr,"No events found for the given parameters."
+            return None
 
+        # Event loop
         for event in events:
-            origin = event.preferred_origin()
-            if origin is None:
-                print >>sys.stderr,"Bad origin."
-                continue
+            print >>sys.stderr,""
+            origin = self.getOrigin(event)
 
-        return
+            print >>sys.stderr,"Working on origin: %s" % str(origin.time)
 
-    def stationBased(self, t0, t1, targetSamplingRate, allowedGainCodes, timeRange,
+            for code in networkStationCodes:
+                (net, sta) = code.split(".")
+
+                inventory = self.arclink_manager.get_inventory(net, sta, "*", "*", (origin.time - 86400).datetime, (origin.time + 86400).datetime)
+                if inventory is None or len(inventory.network) == 0:
+                    print >>sys.stderr,"No stations for pattern: %s.%s" % (net, sta)
+                    continue
+
+                print >>sys.stderr,"\n Stations for pattern: %s.%s" % (net,sta)
+                # Station loop
+                for (ncode, nstart, network) in  unWrapNSLC(inventory.network):
+                    for (scode, sstart, station) in  unWrapNSLC(network.station):
+                        try:
+                            print >>sys.stderr,"  Working on station %s.%s " % (network.code, station.code),
+
+                            delta = util.locations2degrees(station.latitude,
+                                                           station.longitude,
+                                                           origin.latitude,
+                                                           origin.longitude)
+
+                            (ta, slowness) = self.find_arrivalTime(origin.time, delta, origin.depth, phaselist)
+
+                            (z,n,e) = self.__getArcLinkChannelList(station, ta, targetSamplingRate, allowedGainCodes)
+
+                            EI = self.build_event_dictionary(origin.time, origin.latitude, origin.longitude, origin.depth)
+                            SI = self.build_station_dictionary(network.code, station.code, station.latitude, station.longitude, station.elevation)
+                            PI = self.build_pick_dictionary(phasename, ta, slowness)
+
+                            lines.append((ta + timeRange.min(),
+                                          ta + timeRange.max(),
+                                          network.code,
+                                          station.code,
+                                          [z,n,e],
+                                          SI,
+                                          EI,
+                                          PI
+                                         )
+                            )
+                            print >>sys.stderr,"OK!"
+                        except NextItem, e:
+                            print >>sys.stderr,"\n  Skipping: %s" % str(e)
+
+        request = self.organize_by_event(lines)
+
+        return request
+
+    def stationBased(self, t0, t1, targetSamplingRate, allowedGainCodes, timeRange, phasesOrPhaseGroup,
                     networkStationCodes,
                     stationRestrictionArea,
 
@@ -261,9 +189,86 @@ class ArcLinkRequestBuilder(BaseBuilder):
                     depthRange = None,
                     distanceRange = None):
 
+        (phasename, phaselist) = self.resolve_phasenames(phasesOrPhaseGroup)
+        print >>sys.stderr,"Searching using: %s %s" % (phasename, phaselist)
+
         # List of request lines
         lines = []
-        return
+
+        ## Start the Loop
+        # On the given station patterns
+        for code in networkStationCodes:
+            (net, sta) = code.split(".")
+            inventory = self.arclink_manager.get_inventory(net, sta, "*", "*", t0.datetime, t1.datetime)
+
+            if inventory is None or len(inventory.network) == 0:
+                print >>sys.stderr,"No stations for pattern: %s.%s" % (net, sta)
+                continue
+
+            print >>sys.stderr,"Stations for pattern: %s.%s" % (net,sta)
+            # Station Loop
+            for (ncode, nstart, network) in  unWrapNSLC(inventory.network):
+                for (scode, sstart, station) in  unWrapNSLC(network.station):
+                    print >>sys.stderr,"\n Working on station %s.%s" % (network.code, station.code)
+
+                    # Start Build the parameters to pass on the Event Client
+                    start = UTCDateTime(station.start) if station.start else t0
+                    end = UTCDateTime(station.end) if station.end else t1
+                    kwargsevent = self.fill_kwargsevent(max([start, t0]),
+                                                        min([end, t1]),
+                                                        eventRestrictionArea,
+                                                        magnitudeRange,
+                                                        depthRange,
+                                                        station.latitude,
+                                                        station.longitude,
+                                                        distanceRange)
+
+                    try:
+                        events = self.fdsn_client.get_events(**kwargsevent)
+                    except FDSNException,e:
+                        events = None
+
+                    if events is None or len(events) < 0:
+                        print >>sys.stderr,"  No Events Found."
+                        continue
+
+                    # Event loop
+                    for event in events:
+                        try:
+                            origin = self.getOrigin(event)
+
+                            print >>sys.stderr,"  Working on origin: %s" % str(origin.time),
+
+                            delta = util.locations2degrees(station.latitude,
+                                                           station.longitude,
+                                                           origin.latitude,
+                                                           origin.longitude)
+
+                            (ta, slowness) = self.find_arrivalTime(origin.time, delta, origin.depth, phaselist)
+
+                            (z,n,e) = self.__getArcLinkChannelList(station, ta, targetSamplingRate, allowedGainCodes)
+
+                            EI = self.build_event_dictionary(origin.time, origin.latitude, origin.longitude, origin.depth)
+                            SI = self.build_station_dictionary(network.code, station.code, station.latitude, station.longitude, station.elevation)
+                            PI = self.build_pick_dictionary(phasename, ta, slowness)
+
+                            lines.append((ta + timeRange.min(),
+                                      ta + timeRange.max(),
+                                      network.code,
+                                      station.code,
+                                      [z,n,e],
+                                      SI,
+                                      EI,
+                                      PI)
+                                     )
+                            print >>sys.stderr,"OK!"
+                        except NextItem,e:
+                            print >>sys.stderr,"  Skipping: %s" % str(e)
+                            continue
+
+        request = self.organize_by_station(lines)
+
+        return request
 
 if __name__ == "__main__":
     # Get an Instance of the class
@@ -275,14 +280,15 @@ if __name__ == "__main__":
                     targetSamplingRate = 20.0,
                     allowedGainCodes = ["H", "L"],
                     timeRange = Range(-120, 600),
+                    phasesOrPhaseGroup = "pgroup",
 
-                    networkStationCodes = [ "TA.*"],
+                    networkStationCodes = [ "BL.*", "TA.*"],
                     stationRestrictionArea = AreaRange(-150.0, -90.0, 15.0, 60.0),
 
                     eventRestrictionArea = AreaRange(-35.0, -10.0, -55.0, -60.0),
-                    magnitudeRange = Range(6.2, 9.0),
+                    magnitudeRange = Range(6., 9.0),
                     depthRange = Range(0.0, 400.0),
                     distanceRange = None
                     )
-
+#     rb.show_request(req)
 
